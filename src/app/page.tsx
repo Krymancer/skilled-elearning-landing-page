d4e195879d8cb3802c3280293a993614fcff0b49
Júnior Nascimento <junior.nascm@gmail.com>
2023-11-28 11:13:58 -0300

    desktop finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 import Image from 'next/image'
 
 import Button from '@/components/Button'
+import Card from '@/components/Card';
 
 import LogoDark from '@/assets/logo-dark.svg';
 import LogoLight from '@/assets/logo-light.svg';
-import Card from '@/components/Card';
+import HeroDesktop from '@/assets/image-hero-desktop.png';
 
 interface Card {
   title: string;
@@ -44,7 +45,7 @@ const cards: Card[] = [
 
 export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col min-h-screen overflow-hidden'>
       <header className='flex items-center justify-between px-40 py-10'>
         <div className='relative h-[1.73025rem] w-[6.948rem]'>
           <Image src={LogoDark} fill alt='Logo' />
@@ -52,13 +53,28 @@ export default function Home() {
         <Button label="Get Started" onclick={() => { }} />
       </header>
       <main className='flex flex-col flex-grow h-full flex-auto px-40 pb-[8rem]'>
-        <div></div>
-          <div className='grid grid-cols-3 grid-rows-2 gap-x-8 gap-y-20'>
-            <Card title='Check out our most popular courses!' featured />
-            {cards.map((card, index) => (
-              <Card key={index} title={card.title} content={card.content} icon={card.icon} />
-            ))}
+        <div className='flex justify-between gap-40 pb-60 pt-40'>
+          <div className='flex flex-col gap-10 w-[28.5625rem]'>
+            <div className='flex flex-col gap-[1.81rem]'>
+            <h1 className='text-dark-purple text-[3rem] font-extrabold'>Maximize skill, <br/>minimize budget</h1>
+            <p className='text-gray text-[1.125rem] font-medium leading-[1.75rem]'>Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.</p>
+            </div>
+            <div className='w-[10.4375rem]'>
+              <Button label="Get Started" secondary onclick={() => { }} />  
+            </div>
           </div>
+          <div className='relative w-1/2'>
+            <div className='absolute w-[61.97694rem] h-[58.55731rem] -top-[120%] left-[20%] -z-20'>
+              <Image src={HeroDesktop} alt='Hero' className='bg-cover' fill />
+            </div>
+          </div>
+        </div>
+        <div className='grid grid-cols-3 grid-rows-2 gap-x-8 gap-y-20'>
+          <Card title='Check out our most popular courses!' featured />
+          {cards.map((card, index) => (
+            <Card key={index} title={card.title} content={card.content} icon={card.icon} />
+          ))}
+        </div>
       </main>
       <footer className='bg-dark-purple flex items-center justify-between px-40 py-10'>
         <div className='relative h-[1.73025rem] w-[6.948rem]'>
